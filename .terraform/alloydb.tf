d042390564b17192e3401f1189f8b6bd4e96cc60
Mourad GH <mouradelghissassi1992@gmail.com>
2025-02-18 17:11:27 +0100

    update the gcf to only change it when the code is edited

--- .terraform/alloydb.tf
+++ .terraform/alloydb.tf
@@ -1,51 +1,9 @@
-# Enable required APIs
-resource "google_project_service" "alloydb" {
-  for_each = toset([
-    "alloydb.googleapis.com",
-    "compute.googleapis.com",
-    "servicenetworking.googleapis.com"
-  ])
-  service = each.key
-  disable_on_destroy = false
-}
-
-# VPC Network
-resource "google_compute_network" "alloydb_network" {
-  name                    = "alloydb-network"
-  auto_create_subnetworks = false
-  depends_on = [google_project_service.alloydb]
-}
-
-# Subnet
-resource "google_compute_subnetwork" "alloydb_subnet" {
-  name          = "alloydb-subnet"
-  ip_cidr_range = "10.0.0.0/24"
-  region        = var.region
-  network       = google_compute_network.alloydb_network.id
-}
-
-# Private IP range
-resource "google_compute_global_address" "private_ip_alloydb" {
-  name          = "alloydb-private-ip"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = google_compute_network.alloydb_network.id
-}
-
-# VPC Peering
-resource "google_service_networking_connection" "alloydb_vpc_connection" {
-  network                 = google_compute_network.alloydb_network.id
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_alloydb.name]
-}
-
 # AlloyDB Cluster
-resource "google_alloydb_cluster" "default" {
+resource "google_alloydb_cluster" "gigawatt" {
   cluster_id = "alloydb-cluster"
   location   = var.region
   network_config {
-    network = google_compute_network.alloydb_network.id
+    network = "default"
   }
 
   initial_user {
@@ -53,12 +11,11 @@ resource "google_alloydb_cluster" "default" {
     password = var.database_password
   }
 
-  depends_on = [google_service_networking_connection.alloydb_vpc_connection]
 }
 
 # Primary Instance
 resource "google_alloydb_instance" "primary" {
-  cluster       = google_alloydb_cluster.default.name
+  cluster       = google_alloydb_cluster.gigawatt.name
   instance_id   = "alloydb-instance"
   instance_type = "PRIMARY"
 
@@ -66,7 +23,7 @@ resource "google_alloydb_instance" "primary" {
     cpu_count = 2
   }
 
-  depends_on = [google_alloydb_cluster.default]
+  depends_on = [google_alloydb_cluster.gigawatt]
 }
 
 # Create a local file with SQL commands
