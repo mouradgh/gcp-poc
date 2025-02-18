a7567b4b1719857085cdf0adbdc88d3180abc31e
Mourad GH <mouradelghissassi1992@gmail.com>
2025-02-18 17:13:02 +0100

    update the gcf to only change it when the code is edited

--- .terraform/alloydb.tf
+++ .terraform/alloydb.tf
@@ -14,7 +14,7 @@ resource "google_alloydb_cluster" "gigawatt" {
 }
 
 # Primary Instance
-resource "google_alloydb_instance" "primary" {
+resource "google_alloydb_instance" "gigawatt" {
   cluster       = google_alloydb_cluster.gigawatt.name
   instance_id   = "alloydb-instance"
   instance_type = "PRIMARY"
@@ -24,20 +24,5 @@ resource "google_alloydb_instance" "primary" {
   }
 
   depends_on = [google_alloydb_cluster.gigawatt]
-}
-
-# Create a local file with SQL commands
-resource "local_file" "init_db" {
-  filename = "${path.module}/init.sql"
-  content  = <<-EOT
-    CREATE SCHEMA IF NOT EXISTS gigawatt;
-    
-    CREATE TABLE IF NOT EXISTS gigawatt.raw_files (
-      id SERIAL PRIMARY KEY,
-      filename VARCHAR(255) NOT NULL,
-      content jsonb NOT NULL,
-      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
-  EOT
 }
 
