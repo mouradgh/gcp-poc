49fae7681bce42a0592dab98f0a5f06d4b2bb073
Mourad GH <mouradelghissassi1992@gmail.com>
2025-02-18 11:46:11 +0100

    edit bucket name

--- .terraform/gcs.tf
+++ .terraform/gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "poc_test" {
-  name          = "poc_test"
+  name          = "gigawatt_poc_test"
   location      = var.region
   project       = var.project
   uniform_bucket_level_access = true
